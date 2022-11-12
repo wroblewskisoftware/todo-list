916f6e56c33631254ead2e68722c145e20ad79ae
Radoslaw Wroblewski <wroblewski082@gmail.com>
2022-11-12 17:35:11 +0100

    adding new tasks and showing if a task is done

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@
     },
     {
       content: "dopracować pracę",
-      done: false,
+      done: true,
     },
   ];
 
@@ -19,7 +19,9 @@
 
     for (const task of tasks) {
       htmlString += `
-        <li>
+        <li
+        ${task.done ? "class = list__item--done" : ""}
+        >
         ${task.content}
         </li>
         `;
@@ -28,8 +30,32 @@
     document.querySelector(".js-tasks").innerHTML = htmlString;
   };
 
+  const addNewTask = (newTaskContent) => {
+    tasks.push({
+      content: newTaskContent,
+    });
+
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskContent = document.querySelector(".js-newTask").value.trim();
+
+    if (newTaskContent === "") {
+      return;
+    }
+
+    addNewTask(newTaskContent);
+  };
+
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
